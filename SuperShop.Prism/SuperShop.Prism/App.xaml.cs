e15919d96d1d1c8223ccc745f486fead359e1742
Vítor Conceição <vitoralmeidavc@outlook.pt>
2023-07-11 18:02:52 +0100

    Product Detail Page

--- SuperShop.Prism/SuperShop.Prism/App.xaml.cs
+++ SuperShop.Prism/SuperShop.Prism/App.xaml.cs
@@ -34,6 +34,7 @@ namespace SuperShop.Prism
             containerRegistry.RegisterForNavigation<NavigationPage>();
             containerRegistry.RegisterForNavigation<LoginPage, LoginPageViewModel>();
             containerRegistry.RegisterForNavigation<ProductsPage, ProductsPageViewModel>();
+            containerRegistry.RegisterForNavigation<ProductDetailPage, ProductDetailPageViewModel>();
         }
     }
 }
---
+++ SuperShop.Prism/SuperShop.Prism/ItemViewModels/ProductItemViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Prism.Commands;
+using Prism.Navigation;
+using SuperShop.Prism.Models;
+using SuperShop.Prism.Views;
+
+namespace SuperShop.Prism.ItemViewModels
+{
+    public class ProductItemViewModel : ProductResponse
+    {
+        private readonly INavigationService _navigationService;
+        private DelegateCommand _selectProductCommand;
+
+        public ProductItemViewModel(INavigationService navigationService)
+        {
+            _navigationService = navigationService;
+        }
+
+        public DelegateCommand SelectProductCommand => 
+            _selectProductCommand ?? 
+            (_selectProductCommand = new DelegateCommand(SelectProductAsync));
+
+        private async void SelectProductAsync()
+        {
+            NavigationParameters parameters = new NavigationParameters 
+            {
+                {"product", this }
+            };   
+
+            await _navigationService.NavigateAsync(nameof(ProductDetailPage), parameters);  
+        }
+    }
+}

9e55ea0e01cb9ff46c9f6eb841cabbc62f78847b
Vítor Conceição <vitoralmeidavc@outlook.pt>
2023-07-12 13:01:19 +0100

    Product Details Page

--- SuperShop.Prism/SuperShop.Prism/ViewModels/ProductDetailPageViewModel.cs
+++ SuperShop.Prism/SuperShop.Prism/ViewModels/ProductDetailPageViewModel.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using Prism.Commands;
-using Prism.Mvvm;
-using Prism.Navigation;
+﻿using Prism.Navigation;
 using SuperShop.Prism.Models;
 
 namespace SuperShop.Prism.ViewModels
@@ -11,24 +6,24 @@ namespace SuperShop.Prism.ViewModels
     public class ProductDetailPageViewModel : ViewModelBase
     {
         private readonly INavigationService _navigationService;
-        private ProductResponse _product;   
+        private ProductResponse _product;
 
-        public ProductDetailPageViewModel(INavigationService navigationService) : base(navigationService) 
+        public ProductDetailPageViewModel(INavigationService navigationService) : base(navigationService)
         {
             _navigationService = navigationService;
-        }   
+        }
 
         public ProductResponse Product
         {
             get => _product;
-            set => SetProperty(ref _product, value);    
+            set => SetProperty(ref _product, value);
         }
 
         public override void OnNavigatedTo(INavigationParameters parameters)
         {
             base.OnNavigatedTo(parameters);
 
-            if(parameters.ContainsKey("product"))
+            if (parameters.ContainsKey("product"))
             {
                 Product = parameters.GetValue<ProductResponse>("product");
                 Title = Product.Name;
